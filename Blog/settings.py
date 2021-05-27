9d975741943881763dcc1523d3b506ce23b54413
_thera zhang_ <_zyr724725@gmail.com_>
2021-05-27 20:46:04 +0800

    自定义404.500页面

--- Blog/settings.py
+++ Blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^jh^-)dt+co38irly!ly*$5e42-g!xh%=32ne0u1kdq8j+-tcl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'Blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
